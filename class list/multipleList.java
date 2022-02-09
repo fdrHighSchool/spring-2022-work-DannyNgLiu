0f89f52d7314a7c167335d9e1e61abd2ab4e21bb
DannyNgLiu <dannyn5nycstudents.net>
2022-02-09 08:23:59 -0500

    displays name that are close contacts

--- class list/multipleList.java
+++ class list/multipleList.java
@@ -6,9 +6,25 @@ class multipleList {
     String[] studentName = {"Jimmy Smith", "Maria Hernandez", "Jamal Jerkins", "Ramiz Ahmed", "Michael Green"};
     Boolean[] closeContact = {true, false, false, true, true};
     int[][] grades = {{90, 80, 95, 90, 100}, {95, 100, 100, 90, 90}, {85, 85, 95, 100, 90}, {90, 100, 75, 85, 90}, {80, 80, 65, 55, 90}};
-    System.out.println(Arrays.toString(studentID));
-    System.out.println(Arrays.toString(studentName));
-    System.out.println(Arrays.toString(closeContact));
-    System.out.println(Arrays.deepToString(grades));
+    // System.out.println(Arrays.toString(studentID));
+    // System.out.println(Arrays.toString(studentName));
+    // System.out.println(Arrays.toString(closeContact));
+    // System.out.println(Arrays.deepToString(grades));
+
+    int counter = 0;
+    int counter2 = 0;
+    for(int i=0; i<closeContact.length; i++) {
+      if(closeContact[i]) {
+        counter++;
+      }
+    }
+    String[] closeContactStudents = new String[counter];
+    for(int q=0; q<closeContact.length; q++) {
+      if(closeContact[q]) {
+        closeContactStudents[counter2] = studentName[q];
+        counter2++;
+      }
+    }
+    System.out.println(Arrays.toString(closeContactStudents));
   }
 }
