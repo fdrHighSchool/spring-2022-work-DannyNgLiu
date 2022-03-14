b76969ac01d6f94d9d18128696a2ae02b1a698c2
DannyNgLiu <dannyn5@nycstudents.net>
2022-03-14 08:05:42 -0400

    work from home

--- gol/cell.java
+++ gol/cell.java
@@ -1,40 +1,82 @@
 public class cell {
+  private int x;
+  private int y;
   private String status;
-  private array board;
 
-  public cell(int x, int y, String status, array board) {
+  public cell(int x, int y) {
+    this.x= x;
+    this.y = y;
+  }
+
+  public cell(String status) {
     this.status = status;
-    this.array = array;
   }
 
-    public int getStatus() {
-      if(this.status.equals("O")) {
-        return 1; //alive
-      }
-        return -1; //dead
-      }
-    }
-
-    public int countNeighbor(int x, int y, String[][] board) {
-      for(int i = -1; i<=1; i++) {
-        for(int q = -1; q<=1; q++) {
-          if(board[x+i][y+q].getStatus == 1) {
-            counter++;
-          }
-          if(board[x][y].getStatus == 1) {
-            counter--;
-          }
+  public String[][] createBoard() {
+    String[][] board = new String[this.x][this.y];
+    for(int x=0; x<this.x; x++) {
+      for(int y=0; y<this.y; y++) {
+        if(Math.random() >= .2) {
+          board[x][y] = " O ";
+        } else {
+          board[x][y] = " X ";
         }
-
       }
-
-    public int condition() {
-      if()
     }
+    return board;
+  }
 
-
-    public void setStatus() {
-      this.status = this.status * -1;
+  public void displayBoard(String[][] board) {
+    for(int x=0; x<this.x; x++) {
+      for(int y=0; y<this.y; y++) {
+        System.out.print(board[x][y]);
+      }
+      System.out.println("");
     }
   }
+
+  public int countNeighbor(int x, int y, String[][] board) {
+    int count = 0;
+
+    for(int i = -1; i <= 1; i++) {
+      for(int j = -1; j <= 1; j++) {
+        if(board[x + i][y + j].getStatus() == 1) {
+          count++;
+        }
+      }
+    }
+
+    if(board[x][y].getStatus() == 1) {
+      count--;
+    }
+  }
+    public int getStatus() {
+      if(board[x][y].equals(" O ")) {
+        return 1;
+      } else {
+        return -1;
+      }
+    }
+    //
+    // public int countNeighbor(int x, int y, String[][] board) {
+    //   for(int i = -1; i<=1; i++) {
+    //     for(int q = -1; q<=1; q++) {
+    //       if(board[x+i][y+q].getStatus == 1) {
+    //         counter++;
+    //       }
+    //       if(board[x][y].getStatus == 1) {
+    //         counter--;
+    //       }
+    //     }
+    //
+    //   }
+    //
+    // public int condition() {
+    //   if()
+    // }
+    //
+    //
+    // public void setStatus() {
+    //   this.status = this.status * -1;
+    // }
 }
--- gol/main.java
+++ gol/main.java
@@ -1,37 +1,9 @@
 import java.util.*;
 public class main {
   public static void main(String[] args) {
+    cell game = new cell(10,10);
+    cell status = new cell("1");
 
-    String[][] board = new String[10][10];
-    fillBoard(board);
-    displayBoard(board);
-    cell(board);
-    System.out.println(cell.counterNeighbor(5,5,board));
-
-}
-  //creating the board
-  public static void fillBoard(String[][] board) {
-  for(int row = 0; row < board.length; row++) {
-    for(int col = 0; col < board[row].length; col++) {
-      board[row][col] = "[ ]";
-      board[5][5] = " O ";
-
-      // if(Math.random() < 0.2) {
-      //   board[row][col] = " O ";
-      // } else {
-      //   board[row][col] = " X ";
-      // }
-    } // end inner loop
-  } // end outer loop
-} // end fillBoard method
-
-public static void displayBoard(String[][] board) {
-  for(int row = 0; row < board.length; row++) {
-    for(int col = 0; col < board[row].length; col++) {
-      System.out.print(board[row][col] + " ");
-    } // end inner for loop
-    System.out.println();
-  } // end outer for loop
-} // end displayBoard method
-
+    game.displayBoard(game.createBoard());
+  }
 }
